1777835c2c1516f3add64ac4dc2c39f9638383ed
amanr <amanrique068@gmail.com>
2023-04-16 16:34:40 -0500

    cambios

--- src/main/java/com/example/proyectogticsgrupo2/controller/AdministrativoController.java
+++ src/main/java/com/example/proyectogticsgrupo2/controller/AdministrativoController.java
@@ -1,4 +1,12 @@
 package com.example.proyectogticsgrupo2.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AdministrativoController {
+    @GetMapping("/administrativo")
+    public String dashboard(){
+        return "administrativo/index";
+    }
 }
