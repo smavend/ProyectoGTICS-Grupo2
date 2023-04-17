c6c2b642babbdb3d404a7ec4e52143f467a7508b
noe <noe.jaraq@gmail.com>
2023-04-16 19:48:45 -0500

    index paciente agregado y verCitas agregado
    falta arreglar src y hrefs en ambos

--- src/main/java/com/example/proyectogticsgrupo2/controller/PacienteController.java
+++ src/main/java/com/example/proyectogticsgrupo2/controller/PacienteController.java
@@ -1,4 +1,13 @@
 package com.example.proyectogticsgrupo2.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PacienteController {
+
+    @GetMapping(value = {"", "/", "index"})
+    public String index(){
+        return "paciente/index";
+    }
 }
