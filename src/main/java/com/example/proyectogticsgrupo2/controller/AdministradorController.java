7dced8a098eee9ddb3f986005721b1144188c1ba
jorgeluis <jorgel.dominguez03@gmail.com>
2023-04-18 13:39:08 -0500

    plantilla dashboard

--- src/main/java/com/example/proyectogticsgrupo2/controller/AdministradorController.java
+++ src/main/java/com/example/proyectogticsgrupo2/controller/AdministradorController.java
@@ -1,4 +1,17 @@
 package com.example.proyectogticsgrupo2.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/administrador")
 public class AdministradorController {
+
+    @GetMapping("dashboard")
+    public String dashboard (){
+        return "administrador/dashboard";
+    }
+
+
 }
