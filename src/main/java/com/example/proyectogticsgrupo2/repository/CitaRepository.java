f0bfdcde81a248123c680a9ad556b7efe9449164
william24jes <114313836+william24jes@users.noreply.github.com>
2023-04-18 18:51:48 -0500

    citarepository

---
+++ src/main/java/com/example/proyectogticsgrupo2/repository/CitaRepository.java
@@ -0,0 +1,17 @@
+package com.example.proyectogticsgrupo2.repository;
+
+import com.example.proyectogticsgrupo2.entity.Cita;
+import com.example.proyectogticsgrupo2.entity.Doctor;
+import com.example.proyectogticsgrupo2.entity.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CitaRepository extends JpaRepository<Cita, String> {
+    @Query(value = "Select * FROM cita WHERE doctor_id_doctor = ?1",
+            nativeQuery = true) //TENER CUIDADO CON El PUNTO Y COMA AL FINAL DEL QUERY PQ SINO, NO FUNCIONA
+    List<Cita> BuscarPorDoctor(Integer id);
+}
