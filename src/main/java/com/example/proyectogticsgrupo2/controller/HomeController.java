b6e8488a6f45ec434e89ba0672c6250286b52adc
amanr <amanrique068@gmail.com>
2023-04-18 16:45:32 -0500

    vistas general

--- src/main/java/com/example/proyectogticsgrupo2/controller/HomeController.java
+++ src/main/java/com/example/proyectogticsgrupo2/controller/HomeController.java
@@ -9,4 +9,37 @@ public class HomeController {
     public String principal(){
         return "general/home";
     }
+    @GetMapping("/sedes")
+    public String mostrarSedes(){
+        return "general/sedes";
+    }
+    @GetMapping("/login")
+    public String iniciarSesion(){
+        return "general/login";
+    }
+    @GetMapping("/login/olvidepassw")
+    public String pedirCorreo(){
+        return "general/pedirCorreo";
+    }
+    @GetMapping("/login/olvidepassw/confirmacion")
+    public String mensajeCambioContrasenia(){
+        return "general/mensajeSolicitudContrasenia";
+    }
+    @GetMapping("/login/cambiarpassw")
+    public String cambiarContrasenia(){
+        return "general/ingresarContrasenia";
+    }
+    @GetMapping("/login/cambiarpassw/success")
+    public String cambioPasswExitoso(){
+        return "general/confirmacioncontrasenia";
+    }
+    @GetMapping("/signin")
+    public String vistaRegistro(){
+        return "general/registro";
+    }
+
+    @GetMapping("/signin/confirmacion")
+    public String confirmacionRegistro(){
+        return "general/confirmacionregistro";
+    }
 }
