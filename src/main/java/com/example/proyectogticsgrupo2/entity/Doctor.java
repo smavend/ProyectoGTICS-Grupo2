96e9a3581bc100df9a65de96378908edf3681f8f
william24jes <114313836+william24jes@users.noreply.github.com>
2023-04-18 19:01:45 -0500

    Update Doctor.java

--- src/main/java/com/example/proyectogticsgrupo2/entity/Doctor.java
+++ src/main/java/com/example/proyectogticsgrupo2/entity/Doctor.java
@@ -1,5 +1,6 @@
 package com.example.proyectogticsgrupo2.entity;
 
+
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,13 +8,31 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "doctor")
+@Table(name="doctor")
 public class Doctor {
     @Id
-    @Column(name = "id_doctor",nullable = false)
-    private String idDoctor;
+    private String id_doctor;
+    private String nombre;
+    private String apellidos;
+    private String estado;
 
-    @Lob
-    @Column(nullable = false)
-    private byte[] foto;
+    @ManyToOne
+    @JoinColumn(name = "especialidad_id_especialidad")
+    private Especialidad especialidad;
+
+    @ManyToOne
+    @JoinColumn(name = "sede_id_sede")
+    private Sede sede;
+
+    private String duracion_cita_horas;
+
+    @ManyToOne
+    @JoinColumn(name = "horario_id_horario")
+    private Horario horario;
+
+    private String correo;
+    private String foto;
+    private String precio;
+
+
 }
