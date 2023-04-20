d40b9ef9bc524ce08398574b2c630625dbebc4e9
jorgeluis <jorgel.dominguez03@gmail.com>
2023-04-20 00:44:35 -0500

    model a crearPaciente

--- src/main/java/com/example/proyectogticsgrupo2/controller/AdministradorController.java
+++ src/main/java/com/example/proyectogticsgrupo2/controller/AdministradorController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/administrador")
+@RequestMapping("/ ")
 public class AdministradorController {
     final PacienteRepository pacienteRepository;
     final SeguroRepository seguroRepository;
