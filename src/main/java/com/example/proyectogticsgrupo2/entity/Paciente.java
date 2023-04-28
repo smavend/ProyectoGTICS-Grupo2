8319afffd76a380fcba843cf27afc25bce680cef
jorgeluis <jorgel.dominguez03@gmail.com>
2023-04-27 20:46:02 -0500

    validar crear usuario

--- src/main/java/com/example/proyectogticsgrupo2/entity/Paciente.java
+++ src/main/java/com/example/proyectogticsgrupo2/entity/Paciente.java
@@ -7,7 +7,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
+import jakarta.validation.constraints.*;
 
 @Getter
 @Setter
@@ -17,31 +17,50 @@ public class Paciente {
 
     @Id
     @Column(name="id_paciente")
+    @NotBlank(message = "Este campo no puede estar vacío")
+    @Digits(integer = 8, fraction = 0, message = "En DNI debe ser un número y tener 8 dígitos")
     private String idPaciente;
+
+    @Column (nullable = false)
+    @NotBlank(message = "Este campo no puede estar vacío")
+    @Size(max = 45, message = "El nombre no puede tener más de 45 caracteres")
     private String nombre;
+    @Column (nullable = false)
+    @NotBlank(message = "Este campo no puede estar vacío")
+    @Size(max = 45, message = "El apellido no puede tener mas de 45 caracteres")
     private String apellidos;
     @Column (nullable = false)
     private int estado; //no activo: 0, activo: 1,invitado: 2, registrado:3, agendado: 4, en consulta: 5
     @ManyToOne
-    @JoinColumn(name="seguro_id_seguro")
+    @JoinColumn(name="seguro_id_seguro",nullable = false)
     private Seguro seguro;
+    @NotBlank(message = "Este campo no puede estar vacío")
+    @Digits(integer = 9, fraction = 0, message = "El telefono debe ser un número tener 9 dígitos")
     private String telefono;
     @ManyToOne
     @JoinColumn(name="administrativo_id_administrativo")
     private Administrativo administrativo;
+    @Column (nullable = false)
+    @NotBlank(message = "Este campo no puede estar vacío")
+    @Email(message = "Ingrese una dirección de correo válida")
     private String correo;
     private String fotoname;
     private String fotocontenttype;
     private byte[] foto;
     @ManyToOne
     @JoinColumn(name = "distrito_id_distrito", nullable = false)
+
     private Distrito distrito;
     @Column (nullable = false)
+    @NotBlank(message = "Este campo no puede estar vacío")
     private String direccion;
-
+    @Column (nullable = false)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate fechanacimiento;
+    @Column (nullable = false)
+    @NotBlank(message = "Seleccione un género")
     private String genero;
+    @Column (nullable = false)
     private LocalDateTime fecharegistro;
 
 }
--- src/main/java/com/example/proyectogticsgrupo2/repository/PacienteRepository.java
+++ src/main/java/com/example/proyectogticsgrupo2/repository/PacienteRepository.java
@@ -15,9 +15,5 @@ public interface PacienteRepository extends JpaRepository<Paciente, String> {
 
     @Query(nativeQuery = true, value = "SELECT m.*, doctor.nombre as 'nombreemisor' FROM mensaje m inner join doctor on (doctor.id_doctor=m.id_emisor) where id_receptor=?1")
     List<MensajeDatosDto> obtenerMensajeDatos(String id);
-    @Query(nativeQuery = true, value = "INSERT INTO proyectogtics.paciente (id_paciente, nombre, apellidos,estado, seguro_id_seguro, telefono, administrativo_id_administrativo, correo, foto,direccion, distrito_id_distrito) " +
-            "VALUES (?1, ?2, ?3,?4 ?5, " +
-            "?6, ?7, ?8, ?9, ?10, ?11")
-    void guardarPaciente (String dni, String nombre, String apellido, int estado, int numseguro, String telefono, String numadmin,
-                          String correo, Byte foto, String direccion, int numdistrito);
+
 }
