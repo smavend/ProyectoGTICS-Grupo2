698cbaf480befa1140b9cc52382f6063dd62d530
Gustavo <a20192858@pucp.edu.pe>
2023-05-29 16:37:45 -0500

    commit 16

---
+++ src/main/java/com/example/proyectogticsgrupo2/entity/Stylevistas.java
@@ -0,0 +1,30 @@
+package com.example.proyectogticsgrupo2.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "stylevistas")
+public class Stylevistas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "user")
+    private String user;
+
+    @Column(name = "Header")
+    private String header;
+
+    @Column(name = "Footer")
+    private String footer;
+
+    @Column(name = "Background")
+    private String background;
+
+    // Getters y setters
+}
