a8832d75ff2ce0692fbcc0fce027828d64c08780
jorgeluis <jorgel.dominguez03@gmail.com>
2023-05-29 21:43:37 -0500

    crear doctor

---
+++ src/main/java/com/example/proyectogticsgrupo2/dto/ConsentimPaciDto.java
@@ -0,0 +1,5 @@
+package com.example.proyectogticsgrupo2.dto;
+
+public interface ConsentimPaciDto {
+    String getConcentimientospaciente();
+}
---
+++ src/main/java/com/example/proyectogticsgrupo2/dto/TratamientPaciDto.java
@@ -0,0 +1,7 @@
+package com.example.proyectogticsgrupo2.dto;
+
+public interface TratamientPaciDto {
+    String getDiagnosticopaci();
+    String getRecetapaci();
+    String getTratamientopaci();
+}
