43be44a60c313559cb2446c9c21f9cee3e8462a2
noe <noe.jaraq@gmail.com>
2023-06-24 21:49:08 -0500

    carpeta web service

---
+++ src/main/java/com/example/proyectogticsgrupo2/web_service/ReservarCitaController.java
@@ -0,0 +1,10 @@
+package com.example.proyectogticsgrupo2.web_service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(method = RequestMethod.GET, value = "/reservarCita")
+public class ReservarCitaController {
+}
